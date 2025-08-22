a54f59b8f4b2d4c532cc383bb1ba31bf762f3f1c
Vitor <vitordeazambuja@edu.uniube.br>
2025-08-22 14:09:03 -0300

    Initial Commit

---
+++ Saborize/SaborizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaborizeApp.swift
+//  Saborize
+//
+//  Created by Vitor de Azambuja on 22/08/25.
+//
+
+import SwiftUI
+
+@main
+struct SaborizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
