6d72522958eb55637c1ebf9f5c06f4b962390cae
Vitor <vitordeazambuja@edu.uniube.br>
2025-09-03 12:19:11 -0300

    feat: create recipe detail view

---
+++ Saborize/Views/RecipeDetailView.swift
@@ -0,0 +1,53 @@
+//
+//  RecipeDetailView.swift
+//  Saborize
+//
+//  Created by Vitor de Azambuja on 03/09/25.
+//
+
+import SwiftUI
+
+struct RecipeDetailView: View {
+    let recipe: Recipe
+    
+    var body: some View {
+        VStack{
+            HStack{
+                Text("Author: \(recipe.mainInformation.author)")
+                    .font(.subheadline)
+                    .padding()
+                Spacer()
+            }
+            HStack{
+                Text(recipe.mainInformation.description)
+                    .font(.subheadline)
+                    .padding()
+                Spacer()
+            }
+            List{
+                Section(header: Text("Ingredients")){
+                    ForEach(recipe.ingredients.indices, id: \.self){
+                        index in let ingredient = recipe.ingredients[index]
+                        Text(ingredient.description)
+                    }
+                }
+                Section(header: Text("Directions")){
+                    ForEach(recipe.directions.indices, id: \.self){
+                        index in let direction = recipe.directions[index]
+                        HStack{
+                            Text("\(index+1). ").bold()
+                            Text("\(direction.isOptional ? "(Optional) " : "")" + "\(direction.description)")
+                        }
+                    }
+                }
+            }
+        }
+        .navigationTitle(recipe.mainInformation.name)
+    }
+}
+
+#Preview {
+    NavigationView{
+        RecipeDetailView(recipe: Recipe.testRecipes[0])
+    }
+}
