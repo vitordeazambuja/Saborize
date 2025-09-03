658a381302b8d5d4429f8a034a66d3f161feab9d
Vitor <vitordeazambuja@edu.uniube.br>
2025-09-03 13:57:13 -0300

    feat: add foreground and background colors view model

---
+++ Saborize/ViewModels/AppColor.swift
@@ -0,0 +1,13 @@
+//
+//  AppColor.swift
+//  Saborize
+//
+//  Created by Vitor de Azambuja on 03/09/25.
+//
+
+import SwiftUI
+
+struct AppColor{
+    static let foreground: Color = Color(.sRGB,red: 168/255,green: 95/255,blue: 0,opacity: 1)
+    static let background: Color = Color(.sRGB, red: 255/255,green: 248/255,blue: 184/255,opacity: 1)
+}
