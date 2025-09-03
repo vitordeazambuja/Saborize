d77c83ed7fa928845759aca6d136a264945b2bd6
Vitor <vitordeazambuja@edu.uniube.br>
2025-09-03 13:57:49 -0300

    fix: apply new colors to list view

--- Saborize/Views/RecipesListView.swift
+++ Saborize/Views/RecipesListView.swift
@@ -9,12 +9,16 @@ import SwiftUI
 
 struct RecipesListView: View {
     @StateObject var recipeData = RecipeData()
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
     
     var body: some View {
         List{
             ForEach(recipes){
                 recipe in NavigationLink(recipe.mainInformation.name,destination: RecipeDetailView(recipe: recipe))
             }
+            .listRowBackground(listBackgroundColor)
+            .foregroundColor(listTextColor)
         }
         .navigationTitle(navigationTitle)
     }
