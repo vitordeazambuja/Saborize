f216c3c2506abc1234b33956af3944b4a0d04919
Vitor <vitordeazambuja@edu.uniube.br>
2025-09-03 13:57:33 -0300

    fix: apply new colors to detailed view

--- Saborize/Views/RecipeDetailView.swift
+++ Saborize/Views/RecipeDetailView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct RecipeDetailView: View {
     let recipe: Recipe
     
+    private let listBackgroundColor = AppColor.background
+    private let listTextColor = AppColor.foreground
+    
     var body: some View {
         VStack{
             HStack{
@@ -29,17 +32,18 @@ struct RecipeDetailView: View {
                     ForEach(recipe.ingredients.indices, id: \.self){
                         index in let ingredient = recipe.ingredients[index]
                         Text(ingredient.description)
+                            .foregroundColor(listTextColor)
                     }
-                }
+                } .listRowBackground(listBackgroundColor)
                 Section(header: Text("Directions")){
                     ForEach(recipe.directions.indices, id: \.self){
                         index in let direction = recipe.directions[index]
                         HStack{
                             Text("\(index+1). ").bold()
                             Text("\(direction.isOptional ? "(Optional) " : "")" + "\(direction.description)")
-                        }
+                        }.foregroundColor(listTextColor)
                     }
-                }
+                }.listRowBackground(listBackgroundColor)
             }
         }
         .navigationTitle(recipe.mainInformation.name)
