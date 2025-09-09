6d84735fd0cac72fa7700ac09246d9b05dc463cf
Vitor <vitordeazambuja@edu.uniube.br>
2025-09-09 10:52:12 -0300

    structure: create explore recipes and modify recipes view folders

---
+++ Saborize/Views/ModifyRecipes/ModifyRecipeView.swift
@@ -0,0 +1,19 @@
+//
+//  ModifyRecipeView.swift
+//  Saborize
+//
+//  Created by Vitor de Azambuja on 09/09/25.
+//
+
+import SwiftUI
+
+struct ModifyRecipeView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModifyRecipeView()
+}
