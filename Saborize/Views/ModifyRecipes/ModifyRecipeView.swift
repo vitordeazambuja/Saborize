7f2c015b236ee7eb4bd02c46b2b6473be8dbbff8
Vitor <vitordeazambuja@edu.uniube.br>
2025-09-09 11:01:35 -0300

    feat: create view to modify recipe

--- Saborize/Views/ModifyRecipes/ModifyRecipeView.swift
+++ Saborize/Views/ModifyRecipes/ModifyRecipeView.swift
@@ -8,12 +8,18 @@
 import SwiftUI
 
 struct ModifyRecipeView: View {
+    @Binding var recipe: Recipe
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("Fill in the recipe with test data"){
+            recipe.mainInformation = MainInformation(name: "test", description: "test", author: "test", category: .breakfast)
+            recipe.directions = [Direction(description: "test", isOptional: false)]
+            recipe.ingredients = [Ingredient(name: "test", quantity: 1.0, unit: .none)]
+        }
     }
 }
 
 #Preview {
-    ModifyRecipeView()
+    @Previewable @State var recipe = Recipe()
+    ModifyRecipeView(recipe: $recipe)
 }
