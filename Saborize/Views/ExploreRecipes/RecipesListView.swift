162839b980acf8057438c8c189059f6749a6ee73
Vitor <vitordeazambuja@edu.uniube.br>
2025-09-15 10:01:53 -0300

    feat: add modal presentation for adding a new recipe

--- Saborize/Views/ExploreRecipes/RecipesListView.swift
+++ Saborize/Views/ExploreRecipes/RecipesListView.swift
@@ -11,18 +11,54 @@ struct RecipesListView: View {
     @EnvironmentObject private var recipeData: RecipeData
     let category: MainInformation.Category
     
+    @State private var isPresenting = false
+    @State private var newRecipe = Recipe()
+    
     private let listBackgroundColor = AppColor.background
     private let listTextColor = AppColor.foreground
     
     var body: some View {
-        List{
-            ForEach(recipes){
-                recipe in NavigationLink(recipe.mainInformation.name,destination: RecipeDetailView(recipe: recipe))
+        NavigationView {
+            List {
+                ForEach(recipes) { recipe in
+                    NavigationLink(
+                        recipe.mainInformation.name,
+                        destination: RecipeDetailView(recipe: recipe)
+                    )
+                }
+                .listRowBackground(listBackgroundColor)
+                .foregroundColor(listTextColor)
             }
-            .listRowBackground(listBackgroundColor)
-            .foregroundColor(listTextColor)
+            .navigationTitle(navigationTitle)
+            .toolbar(content: {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        isPresenting = true
+                    }, label: {
+                        Image(systemName: "plus")
+                    })
+                }
+            })
+            .sheet(isPresented: $isPresenting, content: {
+                NavigationView{
+                    ModifyRecipeView(recipe: $newRecipe)
+                        .toolbar(content: {
+                            ToolbarItem(placement: .cancellationAction){
+                                Button("Dismiss"){
+                                    isPresenting = false
+                                }
+                            }
+                            ToolbarItem(placement: .confirmationAction) {
+                                Button("Add"){
+                                    recipeData.recipes.append(newRecipe)
+                                    isPresenting = false
+                                }
+                            }
+                        })
+                        .navigationTitle("Add a New Recipe")
+                }
+            })
         }
-        .navigationTitle(navigationTitle)
     }
 }
 
@@ -31,13 +67,13 @@ extension RecipesListView {
         recipeData.recipes(for: category)
     }
     
-    private var navigationTitle: String{
+    private var navigationTitle: String {
         "\(category.rawValue) Recipes"
     }
 }
 
 #Preview {
-    NavigationView{
+    NavigationView {
         RecipesListView(category: .breakfast)
             .environmentObject(RecipeData())
     }
