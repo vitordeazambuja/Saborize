7ecaf7563eef90e4c0818df90dbc7a28061c2baa
Vitor <vitordeazambuja@edu.uniube.br>
2025-09-25 10:56:51 -0300

    feat: add list of ingredients

---
+++ Saborize/Views/ModifyRecipes/ModifyIngredientsView.swift
@@ -0,0 +1,41 @@
+//
+//  ModifyIngredientsView.swift
+//  Saborize
+//
+//  Created by Vitor de Azambuja on 25/09/25.
+//
+
+import SwiftUI
+
+struct ModifyIngredientsView: View {
+    @Binding var ingredients: [Ingredient]
+    @State private var newIngredient = Ingredient(name: "", quantity: 0.0, unit: .none)
+    
+    var body: some View {
+        VStack{
+            if ingredients.isEmpty{
+                Spacer()
+                NavigationLink("Add the first ingredient",destination: ModifyIngredientView(ingredient: $newIngredient))
+                Spacer()
+            }else{
+                List{
+                    ForEach(ingredients.indices, id: \.self){ index in
+                        let ingredient = ingredients[index]
+                        Text(ingredient.description)
+                    }
+                    NavigationLink("Add another ingredient", destination:
+                                    ModifyIngredientView(ingredient: $newIngredient)
+                    )
+                    .buttonStyle(PlainButtonStyle())
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    @Previewable @State var emptyIngredients: [Ingredient] = []
+    NavigationView{
+        ModifyIngredientsView(ingredients: $emptyIngredients)
+    }
+}
