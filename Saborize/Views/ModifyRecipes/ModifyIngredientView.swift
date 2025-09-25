817b40ae5b868d8938cf7c14ad35f3bc904aa47b
Vitor <vitordeazambuja@edu.uniube.br>
2025-09-25 09:45:01 -0300

    feat: add modify ingredient view

---
+++ Saborize/Views/ModifyRecipes/ModifyIngredientView.swift
@@ -0,0 +1,43 @@
+//
+//  ModifyIngredientView.swift
+//  Saborize
+//
+//  Created by Vitor de Azambuja on 25/09/25.
+//
+
+import SwiftUI
+
+struct ModifyIngredientView: View {
+    @State var ingredient: Ingredient
+    var body: some View {
+        VStack{
+            Form{
+                TextField("Ingredient Name", text: $ingredient.name)
+                Stepper(value: $ingredient.quantity, in: 0...100,step: 1.0){
+                    HStack{
+                        Text("Quantity:")
+                        TextField("Quantity", value: $ingredient.quantity,formatter: NumberFormatter())
+                            .keyboardType(.numbersAndPunctuation)
+                    }
+                }
+                Picker(selection: $ingredient.unit, label:
+                        HStack{
+                    Text("Unit")
+                    Spacer()
+                    Text(ingredient.unit.rawValue)
+                }) {
+                    ForEach(Ingredient.Unit.allCases, id: \.self){ unit in
+                        Text(unit.rawValue)
+                    }
+                }
+                .pickerStyle(MenuPickerStyle())
+            }
+        }
+    }
+}
+    
+#Preview {
+    NavigationView {
+        ModifyIngredientView(ingredient: Ingredient(name: "", quantity: 1.0, unit: .none))
+    }
+}
