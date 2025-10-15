a84c64b437b9c44df09002cab7416d75386a8c82
Vitor <vitordeazambuja@edu.uniube.br>
2025-10-15 15:16:28 -0300

    chore: present edit toolbar view

--- Saborize/Views/ExploreRecipes/RecipeDetailView.swift
+++ Saborize/Views/ExploreRecipes/RecipeDetailView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct RecipeDetailView: View {
     @Binding var recipe: Recipe
     
+    @State private var isPresenting = false
+    
     private let listBackgroundColor = AppColor.background
     private let listTextColor = AppColor.foreground
     
@@ -48,6 +50,28 @@ struct RecipeDetailView: View {
             }
         }
         .navigationTitle(recipe.mainInformation.name)
+        .toolbar{
+            ToolbarItem{
+                HStack{
+                    Button("Edit"){
+                        isPresenting = true
+                    }
+                }
+            }
+        }
+        .sheet(isPresented: $isPresenting) {
+            NavigationView{
+                ModifyRecipeView(recipe: $recipe)
+                    .toolbar {
+                        ToolbarItem(placement: .confirmationAction){
+                            Button("Save"){
+                                isPresenting = false
+                            }
+                        }
+                    }
+                    .navigationTitle("Edit Recipe")
+            }
+        }
     }
 }
 
